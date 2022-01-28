1dd84e108f4ba685d6de90a56c52d1c45bc8b042
shonmacray <shonmacray@gmail.com>
2022-01-28 13:00:06 +0300

    config: port

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 import express from "express";
-import aboutme from "./aboutme.json"
-import Person from "./utils.js"
+import dotenv from "dotenv";
+import aboutme from "./aboutme.json";
+import Person from "./utils.js";
+
+dotenv.config();
+const port = process.env.PORT || 2013;
 
 const app = express();
 
 const person = new Person(aboutme);
 
 // check valid doc?
-app.use(function isValidDoc(req, res, next){
-  if(aboutme && "profile" in aboutme && "anchors" in aboutme){
+app.use(function isValidDoc(req, res, next) {
+  if (aboutme && "profile" in aboutme && "anchors" in aboutme) {
     next();
-  }else{
-    res.status(500).send("document not valid")
+  } else {
+    res.status(500).send("document not valid");
   }
-})
+});
 
-app.get("/", (req, res) => person.getAll(req, res))
-app.get("/profile", (req, res) => person.getProfile(req, res))
-app.get("/links", (req, res) => person.getLinks(req, res))
-app.get("/link/:id", (req, res) => person.getLinkContent(req, res))
+app.get("/", (req, res) => person.getAll(req, res));
+app.get("/profile", (req, res) => person.getProfile(req, res));
+app.get("/links", (req, res) => person.getLinks(req, res));
+app.get("/link/:id", (req, res) => person.getLinkContent(req, res));
 
-app.listen(2013, ()=> console.log("fuckin started"))
+app.listen(port, () => console.log(`${port} started`));
